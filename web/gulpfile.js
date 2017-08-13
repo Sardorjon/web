69bb7506f206e3fc20fa429facd815bcee2895f2
Sardorjon <temirov.sardor@mail.ru>
2017-08-13 11:58:49 +0200

    file replacement

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,14 +1,19 @@
 var gulp = require ('gulp'),
 watch = require ('gulp-watch');
 
-gulp.task('default', function( ){
-  console.log('hooray - you created a gulp task.');
-
 gulp.task('html', function( ){
     console.log('something useful created to ur html file.');
+});
+
+gulp.task('styles', function (){
+  return gulp.src('./assets/styles/styles.css')
+    .pipe(gulp.dest('assets/temp/styles'));
 });
 gulp.task('watch', function(){
   watch('.assets/index.html', function(){
     gulp.start('html');
+  });
+  watch('assets/styles/**/*.css', function(){
+    gulp.start('styles');
   });
 });
