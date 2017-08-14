0f4c1d6301e0ef891bcfb970ef8e822c7398a36f
Sardorjon <temirov.sardor@mail.ru>
2017-08-14 16:28:18 +0200

    gulp plugins experiement

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require ('gulp'),
-watch = require ('gulp-watch');
+watch = require ('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 gulp.task('html', function( ){
     console.log('something useful created to ur html file.');
@@ -7,7 +11,7 @@ gulp.task('html', function( ){
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    /*.pipe(postcss([cssvars, nested, autoprefixer]))*/
+    .pipe(postcss([cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 gulp.task('watch', function() {
